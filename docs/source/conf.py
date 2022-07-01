5c4075623ac1080b5479782f463e232cabb644eb
Eunseo Choi <echoi@users.noreply.github.com>
2022-06-30 20:48:57 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'des3d'
+copyright = '2022, Eunseo Choi'
+author = 'Eunseo Choi'
 
 release = '0.1'
 version = '0.1.0'
